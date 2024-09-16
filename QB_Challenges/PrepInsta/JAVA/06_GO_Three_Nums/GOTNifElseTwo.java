a5c6c4434d2ba9d22906ed57788f75c2d43e945d
gchougale <135229221+Ganesh-Chougale@users.noreply.github.com>
2024-09-16 14:38:42 +0530

    Add files via upload

---
+++ QB_Challenges/PrepInsta/JAVA/06_GO_Three_Nums/GOTNifElseTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GOTNifElseTwo {
+    public static void GOTN() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+        sc.close();
+
+    }
+
+    public static void main(String[] args) {
+        GOTN();
+    }
+
+}

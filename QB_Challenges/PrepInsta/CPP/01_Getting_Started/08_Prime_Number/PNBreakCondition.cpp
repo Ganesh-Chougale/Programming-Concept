e53871c20033b4b37dca7304180331bfe6c1ea74
Ganesh-Chougale <gchougale32@gmail.com>
2024-09-22 18:33:15 +0530

    message

---
+++ QB_Challenges/PrepInsta/CPP/01_Getting_Started/08_Prime_Number/PNBreakCondition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void PN(){
+    int num;
+    cout <<"Enter The Number: ";
+    cin >>num;
+
+    bool isPrime = true;
+
+    
+}
+
+int main(){
+
+    return 0;
+}
